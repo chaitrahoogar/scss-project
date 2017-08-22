2f1167a23d68130d7e746def7734f08823964536
Chaitra <chaitra.hoogar@hashworks.co>
2017-08-22 11:15:42 +0530

    changes

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -10,12 +10,12 @@ export class LogInComponent implements OnInit {
    login: any ={};
    token="";
      submit(){
-        // console.log(this.login);
-        // // this.token="anushya";
-        // localStorage.setItem('currentUser', JSON.stringify({ token: this.token }));
-        // var currentUser = JSON.parse(localStorage.getItem('currentUser'));
-        // this.token = currentUser.token; // your token
-        // alert(currentUser.token);
+        console.log(this.login);
+        this.token="anushya";
+        localStorage.setItem('currentUser', JSON.stringify({ token: this.token }));
+        var currentUser = JSON.parse(localStorage.getItem('currentUser'));
+        this.token = currentUser.token; // your token
+        alert(currentUser.token);
         if(this.login.name==undefined ||this.login.password==undefined )
           {
             this.router.navigateByUrl('/login');
